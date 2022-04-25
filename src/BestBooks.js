b8276cd83a12bd2cfbc4d60830a370de87378647
Anthony Hill <anthonymhill23@gmail.com>
2022-04-25 17:18:41 -0400

    added carousel import as well as book state updater

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,116 +1,57 @@
 import React from 'react';
-
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
-      showModal: false,
-      // showUpdateModal: false,
-    };
-    this.closeModal = this.closeModal.bind();
+    }
   }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const bookMaker = {
-      title: e.target.title.value,
-      description: e.target.description.value,
-      read: e.target.read.value,
-      email: e.target.email.value,
-    };
-    console.log(bookMaker);
-    this.makeBook(bookMaker);
-    // e.target.reset()
-  };
-
-  handleClick = () => {
-    this.setState({ showModal: true });
-  };
-
-  closeModal = () => {
-    this.setState({ showModal: false });
-  };
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
-  getBooks = async () => {
-    let url = `${process.env.REACT_APP_LOCALHOST}/books?`;
-    if (this.props.user) {
-      url = `${process.env.REACT_APP_LOCALHOST}/books?email=${this.props.user.email}`;
-    }
-    try {
-      let result = await axios.get(url);
-      console.log(result)
-      console.log("result", result.data);
-      this.setState({ books: result.data });
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  deleteBook = async (id) => {
-    console.log('id in deleteBook: ', id)
-    try {
-      await axios.delete(`${process.env.REACT_APP_LOCALHOST}/books/${id}?email=${this.props.user.email}`);
-
-      const updatedBooks = this.state.books.filter(book => book._id !== id);
-      this.setState({ books: updatedBooks });
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  postBook = async (userBook) => {
-    try {
-      const bookResponse = await axios.post(process.env.REACT_APP_LOCALHOST + '/books', userBook);
-      this.setState({ books: [...this.state.books, bookResponse.data] });
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  updateBook = async (updatedBook, id) => {
-    console.log('updatedBook: ', updatedBook)
-    console.log('id: ', id)
-    // this.setState({showUpdateModal: true})
-
-    try {
-      const updateBook = await axios.put(`${process.env.REACT_APP_LOCALHOST}/books/${id}${updatedBook}`);
-      // const updateBook = await axios.put(`${process.env.REACT_APP_LOCALHOST}/books/${updatedBook}`);
-      const newBookState = this.state.books.map(book => {
-        if (book._id === id) {
-          return updateBook.data;
-        }
-        return book;
+fetchBooks = async () => {
+  try{
+    let url = `${process.env.REACT_APP_SERVER}/books`
+    console.log(url)
+    const response = await axios.get(url)
+      ;
+      console.log(response);
+      this.setState({
+        books: response.data
       })
-      this.setState({ books: newBookState })
-    } catch (e) {
-      console.error(e)
-    }
+      console.log(this.state.books)
+  } catch(error) {
+    console.log('error', error.response)
   }
+}
 
-  componentDidMount() {
-    this.getBooks();
-  }
+componentDidMount(){
+  this.fetchBooks();
+}
 
   render() {
+
+    /* TODO: render user's books in a Carousel */
+    let carouselItems = this.state.books.map(book => (
+      <Carousel.Item key={book._id}>
+        <p>{book.title}</p>
+        <p>{book.description}</p>
+      </Carousel.Item>
+    ))
+
     return (
       <>
-        <h2>Michael and Anthony's Digital Book Emporium </h2>
+        <h2>Anthony and Michaels Lifelong learning and reading emporium</h2>
 
         {this.state.books.length ? (
-          <BookCarousel deleteBook={this.deleteBook} books={this.state.books} updateBook={this.updateBook} showModal={this.state.showModal} onClick={this.handleClick} closeModal={this.closeModal} postBook={this.postBook} />
+          <Carousel>
+              {carouselItems}
+            </Carousel>
         ) : (
-          <p>Sorry, there are no books in this collection</p>
+          <h3>No Books Found :(</h3>
         )}
-        {this.state.showModal ? (
-          <BookFormModal showModal={this.state.showModal} onClick={this.handleClick} closeModal={this.closeModal} postBook={this.postBook} />
-        ) : (
-          <Button onClick={this.handleClick}>Add a book</Button>
-        )}
-
-
       </>
     )
   }
