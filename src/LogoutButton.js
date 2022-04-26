e67636f92003a69a68c01e3d4706ec1006ee9645
Anthony Hill <anthonymhill23@gmail.com>
2022-04-26 18:16:22 -0400

    rename file

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -4,7 +4,7 @@ class LogoutButton extends Component {
 
   render() {
     return (
-      <button onClick={this.props.onLogout}>
+      <button className="loginButton" onClick={this.props.onLogout}>
         Log Out
       </button>
     );
