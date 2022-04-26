741411d046c9052c5c3f93858d1b4731d9ab78e4
Anthony Hill <anthonymhill23@gmail.com>
2022-04-26 17:04:48 -0400

    added header for proper book modal

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,57 +1,112 @@
 import React from 'react';
 import axios from 'axios';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Button, CarouselItem, Modal } from 'react-bootstrap';
+import BookFormModal from './BookFormModal';
+import Book from './Book';
+import './BestBooks.css';
+
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
+      bookformModal: false,
     }
   }
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
-fetchBooks = async () => {
-  try{
-    let url = `${process.env.REACT_APP_SERVER}/books`
-    console.log(url)
-    const response = await axios.get(url)
-      ;
+  fetchBooks = async () => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`
+      console.log(url)
+      const response = await axios.get(url)
+        ;
       console.log(response);
       this.setState({
         books: response.data
       })
       console.log(this.state.books)
-  } catch(error) {
-    console.log('error', error.response)
+    } catch (error) {
+      console.log('error', error.response)
+    }
   }
-}
 
-componentDidMount(){
-  this.fetchBooks();
-}
+  burnBook = async (id) => {
+    console.log(id);
+    try{
+      await axios.delete(`${process.env.REACT_APP_SERVER}/books/${id}`);
+      let updatedBooks = this.state.books.filter(book => book._id !== id);
+      this.setState ({
+        books: updatedBooks,
+      })
+    }catch(error){
+      console.log(error.response)
+    }
+  }
+
+  addBook = async (newBook) => {
+    try {
+      let createdBook = await axios.post(`${process.env.REACT_APP_SERVER}/books`, newBook);
+      this.setState({
+        books: [...this.state.books, createdBook.data]
+      })
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+  showForm = () => {
+    this.setState({
+      bookformModal: true,
+    })
+  }
+  onHide = () => {
+    this.setState({
+      bookformModal: false,
+    })
+  }
+
+  componentDidMount() {
+    this.fetchBooks();
+  }
 
   render() {
 
     /* TODO: render user's books in a Carousel */
     let carouselItems = this.state.books.map(book => (
-      <Carousel.Item key={book._id}>
-        <p>{book.title}</p>
-        <p>{book.description}</p>
+      <Carousel.Item className="carouselItem" key={book._id}>
+        <Book 
+          book = {book}
+          burnBook = {this.burnBook}
+        />
       </Carousel.Item>
     ))
 
     return (
       <>
-        <h2>Anthony and Michaels Lifelong learning and reading emporium</h2>
+        <h2>Michael's and Anthony's Book Emporium</h2>
 
         {this.state.books.length ? (
-          <Carousel>
-              {carouselItems}
-            </Carousel>
+          <Carousel >
+            {carouselItems}
+            <CarouselItem className="carouselContainer">
+              <Button className="addBookButton" onClick={this.showForm}>Add New Book</Button>
+            </CarouselItem>
+          </Carousel>
+
         ) : (
-          <h3>No Books Found :(</h3>
+          <>
+            <h3>No Books Found :(</h3>
+            <Button className="addBookButton" onClick={this.showForm}>Add New Book</Button>
+          </>
         )}
+        <Modal show={this.state.bookformModal} onHide={this.onHide}>
+          <Modal.Header className="modal-head"closeButton>
+            <Modal.Title>Add a Book</Modal.Title>
+          </Modal.Header>
+          <BookFormModal
+            addBook={this.addBook} onHide={this.onHide} />
+        </Modal>
       </>
     )
   }
