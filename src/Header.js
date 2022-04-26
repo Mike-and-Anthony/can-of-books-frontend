f9fe92b576839bf438363f3f74d75ef80d1b5cbf
Anthony Hill <anthonymhill23@gmail.com>
2022-04-26 18:17:50 -0400

    added logout button

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import './Header.css';
+import LogoutButton from './LogoutButton'
 
 class Header extends React.Component {
   render() {
@@ -8,7 +10,10 @@ class Header extends React.Component {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        {/* PLACEHOLDER: render a navigation link to the about page */}
+        {/* TODO: if the user is logged in, render a navigation link to profile page */}
+        <NavItem>{this.props.user?<Link to="/Profile">Profile</Link>:''}</NavItem>
+        {/* TODO: if the user is logged in, render the `LogoutButton` */}
+        <NavItem>{this.props.user?<LogoutButton onLogout={this.props.onLogout}/>:''}</NavItem>
       </Navbar>
     )
   }
